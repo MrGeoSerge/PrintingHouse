8619068dc0618596dccac50c8751bbe61ed09352
MrGeoSerge <velychko.serge@gmail.com>
2018-04-27 17:49:46 +0300

    Writing - Reading pricelists added

--- PrintingHouse.ConsoleApp/Program.cs
+++ PrintingHouse.ConsoleApp/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrintingHouse.Domain.Entities.PriceLists;
 
 namespace PrintingHouse.ConsoleApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			#region CreateAndWrite to JSON files
+			//var corosetPriceList = new CorosetPriceList();
+			//var priceListHelper = new PriceListHelper();
+			//priceListHelper.WriteToFile(corosetPriceList, "Coroset");
+			#endregion
+
+			#region
+			//var corosetPriceList = new CorosetPriceList();
+			var priceListHelper = new PriceListHelper<CorosetPriceList>();
+			CorosetPriceList corosetPriceList = priceListHelper.ReadFromFile("CorosetPriceList");
+			#endregion
+
+			Console.ReadKey();
+		}
+	}
 }
---
+++ PrintingHouse.Domain/Entities/PriceLists/PriceListHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+namespace PrintingHouse.Domain.Entities.PriceLists
+{
+	public class PriceListHelper<T>
+	{
+
+		public void WriteToFile(T corosetPriceList, string fileName)
+		{
+			var json = new JavaScriptSerializer().Serialize(corosetPriceList);
+			string path = @"D:\MyApps\PrintingHouse\PrintingHouse.Domain\Data\" + fileName + ".json";
+			File.WriteAllText(path, json);
+		}
+
+		public T ReadFromFile(string fileName)
+		{
+			string path = @"D:\MyApps\PrintingHouse\PrintingHouse.Domain\Data\" + fileName + ".json";
+			var json = File.ReadAllText(path);
+            var serializer = new JavaScriptSerializer();
+            T corosetPriceList1 = serializer.Deserialize<T>(json);
+			return corosetPriceList1;
+		}
+	}
+}
