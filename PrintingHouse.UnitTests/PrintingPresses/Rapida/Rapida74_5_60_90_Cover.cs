24e9c965c6bf0ac90a4e64ab2e1d9dc46807dfd8
Сергей-HP\Сергей <velychko.serge@gmail.com>
2018-04-30 00:16:14 +0300

    Rapida Tests refactoring

--- PrintingHouse.UnitTests/PrintingPresses/Rapida/Rapida74_5_60_90_Cover.cs
+++ PrintingHouse.UnitTests/PrintingPresses/Rapida/Rapida74_5_60_90_Cover.cs
@@ -4,122 +4,108 @@ using PrintingHouse.Domain.Specifications;
 using PrintingHouse.Domain.Entities.Tasks;
 using PrintingHouse.Domain.Entities.BookComponents;
 using PrintingHouse.Domain.Entities.Paper;
+using PrintingHouse.UnitTests.VerificationResults;
 
 namespace BookProduction
 {
 	[TestFixture]
-    public class Rapida74_5_60_90_Cover
-    {
-        // R60C означает Rapida Format 60*90 Cover (обложка)
+	[Category("Rapida74_5_60_90_Cover")]
+	public class Rapida74_5_60_90_Cover
+	{
+		Rapida74_5 rapida;
+		Rapida74_5_60_90_CoverResult rapidaResult;
+		//imposition - спуск
+		//impression - оттиск
 
-        Rapida74_5 rapida;
+		[SetUp]
+		public void Initialize()
+		{
+			rapida = new Rapida74_5(new TaskToPrint(new BookPart("Cover",
+				new IssueFormat(60, 90, 16), new PaperInSheets(PaperType.CoatedPaper, 115, 1.50792, "Unknown", 64, 90),
+				new IssueColors(4, 4), 4), 4400));
 
-        //imposition - спуск
-        //impression - оттиск
+			rapidaResult = new Rapida74_5_60_90_CoverResult();
+		}
 
-        [SetUp]
-        public void Initialize()
-        {
-            rapida = new Rapida74_5(new TaskToPrint(new BookPart("Cover",
-                new IssueFormat(60, 90, 16), new PaperInSheets(PaperType.CoatedPaper, 115, 1.50792, "Unknown", 64, 90),
-                new IssueColors(4, 4), 4), 4400));
-        }
- 
-        //Проверка правильности получения значений из прайса
-        [Test]
-        public void R60C_00_GetFormPriceValue()
-        {
-            Assert.AreEqual(108, rapida.GetFormPriceValue());
-        }
+		//Проверка правильности получения значений из прайса
+		[Test]
+		public void R60C_00_GetFormPriceValue() 
+			=> Assert.AreEqual(expected: rapidaResult.FormPrice, 
+				actual: rapida.GetFormPriceValue());
 
-        [Test]
-        public void R60C_00_GetFittingPriceValue()
-        {
-            Assert.AreEqual(350, rapida.GetFittingPriceValue());
-        }
+		[Test]
+		public void R60C_00_GetFittingPriceValue() 
+			=> Assert.AreEqual(expected: rapidaResult.FittingPriceValue, 
+				actual: rapida.GetFittingPriceValue());
 
-        [Test]
-        public void R60C_00_GetTechNeedsPriceValue()
-        {
-            Assert.AreEqual(0, rapida.GetTechNeedsPriceValue());
-        }
+		[Test]
+		public void R60C_00_GetTechNeedsPriceValue() 
+			=> Assert.AreEqual(expected: rapidaResult.TechNeedsPrice, 
+				actual: rapida.GetTechNeedsPriceValue());
 
-        [Test]
-        public void R60C_00_GetImpressionPriceValue()
-        {
-            Assert.AreEqual(0.059, rapida.GetImpressionPriceValue());
-        }
+		[Test]
+		public void R60C_00_GetImpressionPriceValue() 
+			=> Assert.AreEqual(expected: rapidaResult.ImpressionPrice, 
+				actual: rapida.GetImpressionPriceValue());
 
-        [Test]
-        public void R60C_01_GetPagesPerOneImposition()
-        {
-            Assert.AreEqual(8, rapida.GetPagesPerOneImposition());
-        }
+		[Test]
+		public void R60C_01_GetPagesPerOneImposition() 
+			=> Assert.AreEqual(expected: rapidaResult.PagesPerOneImposition, 
+				actual: rapida.GetPagesPerOneImposition());
 
-        [Test]
-        public void R60C_02_GetImpositionsPerBook()
-        {
-            Assert.AreEqual(0.5, rapida.GetImpositionsPerBook());
-        }
+		[Test]
+		public void R60C_02_GetImpositionsPerBook() 
+			=> Assert.AreEqual(expected: rapidaResult.ImposiotionsPerBook, 
+				actual: rapida.GetImpositionsPerBook());
 
-        [Test]
-        public void R60C_03_GetPrintingSheetsPerBook()
-        {
-            Assert.AreEqual(0.25, rapida.GetPrintingSheetsPerBook());
-        }
+		[Test]
+		public void R60C_03_GetPrintingSheetsPerBook() 
+			=> Assert.AreEqual(expected: rapidaResult.PrintingSheetsPerBook, 
+				actual: rapida.GetPrintingSheetsPerBook());
 
-        [Test]
-        public void R60C_09_GetPrintingSheetsPerPrintRun()
-        {
-            Assert.AreEqual(1100, rapida.GetPrintingSheetsPerPrintRun());
-        }
+		[Test]
+		public void R60C_09_GetPrintingSheetsPerPrintRun() 
+			=> Assert.AreEqual(expected: rapidaResult.PrintingSheetsPerPrintRun, 
+				actual: rapida.GetPrintingSheetsPerPrintRun());
 
-        [Test]
-        public void R60C_04_GetPrintingForms()
-        {
-            Assert.AreEqual(8, rapida.GetPrintingForms());
-        }
+		[Test]
+		public void R60C_04_GetPrintingForms() 
+			=> Assert.AreEqual(expected: rapidaResult.PrintingForms, 
+				actual: rapida.GetPrintingForms());
 
-        [Test]
-        public void R60C_05_GetCostOfPrintingFoms()
-        {
-            Assert.AreEqual(864, rapida.GetCostOfPrintingFoms());
-        }
+		[Test]
+		public void R60C_05_GetCostOfPrintingFoms() 
+			=> Assert.AreEqual(expected: rapidaResult.CostOfPrintingFoms, 
+				actual: rapida.GetCostOfPrintingFoms());
 
-        [Test] //Оттиски
-        public void R60C_06_GetImpressions()
-        {
-            Assert.AreEqual(8800, rapida.GetImpressions());
-        }
+		[Test] //Оттиски
+		public void R60C_06_GetImpressions() 
+			=> Assert.AreEqual(expected: rapidaResult.Impressions, 
+				actual: rapida.GetImpressions());
 
-        [Test]
-        public void R60C_07_GetCostOfImpressions()
-        {
-            Assert.AreEqual(519.2, rapida.GetCostOfImpressions(), delta: 0.01 );
-        }
+		[Test]
+		public void R60C_07_GetCostOfImpressions() 
+			=> Assert.AreEqual(expected: rapidaResult.CostOfImpressions, 
+				actual: rapida.GetCostOfImpressions(), delta: 0.01);
 
-        [Test]
-        public void R60C_08_GetCostOfPrinting()
-        {
-            Assert.AreEqual(1383.2, rapida.GetCostOfPrinting(), delta: 0.01);
-        }
+		[Test]
+		public void R60C_08_GetCostOfPrinting() 
+			=> Assert.AreEqual(expected: rapidaResult.CostOfPrinting, 
+				actual: rapida.GetCostOfPrinting(), delta: 0.01);
 
-        [Test]
-        public void R60C_10_GetPaperConsumptionForTechnicalNeeds()
-        {
-            Assert.AreEqual(0, rapida.GetPaperConsumptionForTechnicalNeeds());
-        }
+		[Test]
+		public void R60C_10_GetPaperConsumptionForTechnicalNeeds() 
+			=> Assert.AreEqual(expected: rapidaResult.PaperConsumptionForTechnicalNeeds, 
+				actual: rapida.GetPaperConsumptionForTechnicalNeeds());
 
-        [Test]
-        public void R60C_11_GetFittingOnPrintRun()
-        {
-            Assert.AreEqual(350, rapida.GetFittingOnPrintRun());
-        }
+		[Test]
+		public void R60C_11_GetFittingOnPrintRun() 
+			=> Assert.AreEqual(expected: rapidaResult.FittingOnPrintRun, 
+				actual: rapida.GetFittingOnPrintRun());
 
-        [Test]
-        public void R60C_12_GetTotalPaperConsumptionInPressFormat()
-        {
-            Assert.AreEqual(1450, rapida.GetTotalPaperConsumptionInPressFormat());
-        }
-    }
+		[Test]
+		public void R60C_12_GetTotalPaperConsumptionInPressFormat() 
+			=> Assert.AreEqual(expected: rapidaResult.TotalPaperConsumptionInPressFormat, 
+				actual: rapida.GetTotalPaperConsumptionInPressFormat());
+	}
 }
---
+++ PrintingHouse.UnitTests/VerificationResults/Rapida74_5_60_90_CoverResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingHouse.UnitTests.VerificationResults
+{
+	public class Rapida74_5_60_90_CoverResult
+	{
+		public int FormPrice => 108;
+		public int FittingPriceValue => 350;
+		public double TechNeedsPrice => 0;
+		public double ImpressionPrice => 0.059;
+		public int PagesPerOneImposition => 8;
+		public double ImposiotionsPerBook => 0.5;
+		public double PrintingSheetsPerBook => 0.25;
+		public int PrintingSheetsPerPrintRun => 1100;
+		public int PrintingForms => 8;
+		public int CostOfPrintingFoms => 864;
+		public int Impressions => 8800;
+		public double CostOfImpressions => 519.2;
+		public double CostOfPrinting => 1383.2;
+		public int PaperConsumptionForTechnicalNeeds => 0;
+		public int FittingOnPrintRun => 350;
+		public int TotalPaperConsumptionInPressFormat => 1450;
+	}
+}
